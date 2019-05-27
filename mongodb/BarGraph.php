41224ed1228282931f2ce1e33626054176d66294
DESKTOP-MU1G3JS\cjw72 <cjw7242@gmail.com>
2019-05-27 18:54:39 +0900

    5.27
    그룹 바 추가

--- mongodb/BarGraph.php
+++ mongodb/BarGraph.php
@@ -5,68 +5,72 @@
 <script src="https://d3js.org/d3.v4.js"></script> 
 </head>
 <body>
-<div id="my_dataviz">
-<script>
 
+    <?php
+        include "getdata.php";
+        //강수량 데이터
+        $waterData = YearDayWaterData($collection);
 
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 90, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+    ?>
+    <div id="my_dataviz">
+        <script>
+            //강수량 데이터
+            var waterData = <?php echo json_encode($waterData) ?>;
+            // set the dimensions and margins of the graph
+            var margin = {top: 10, right: 30, bottom: 90, left: 40},
+                width = 460 - margin.left - margin.right,
+                height = 450 - margin.top - margin.bottom;
+            var color = d3.scaleOrdinal(d3.schemeCategory20);
+            // append the svg object to the body of the page
+            var svg = d3.select("#my_dataviz")
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
 
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+            // Parse the Data
 
-// Parse the Data
-d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv", function(data) {
+            // X axis
+            var x = d3.scaleBand()
+            .range([ 0, width ])
+            .domain(waterData.map(function(d) { return d.Year; }))
+            .padding(0.2);
+            svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+                .attr("transform", "translate(-10,0)rotate(-45)")
+                .style("text-anchor", "end");
 
-// X axis
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(data.map(function(d) { return d.Country; }))
-  .padding(0.2);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
-  .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+            // Add Y axis
+            var y = d3.scaleLinear()
+            .domain([0, 14])
+            .range([ height, 0]);
+            svg.append("g")
+            .call(d3.axisLeft(y));
 
-// Add Y axis
-var y = d3.scaleLinear()
-  .domain([0, 13000])
-  .range([ height, 0]);
-svg.append("g")
-  .call(d3.axisLeft(y));
+            // Bars
+            svg.selectAll("mybar")
+            .data(waterData)
+            .enter()
+            .append("rect")
+                .attr("x", function(d) { return x(d.Year); })
+                .attr("width", x.bandwidth())
+                .attr("fill",function(d,i){return color(i);})
+                // no bar at the beginning thus:
+                .attr("height", function(d) { return height - y(0); }) // always equal to 0
+                .attr("y", function(d) { return y(0); })
 
-// Bars
-svg.selectAll("mybar")
-  .data(data)
-  .enter()
-  .append("rect")
-    .attr("x", function(d) { return x(d.Country); })
-    .attr("width", x.bandwidth())
-    .attr("fill", "#69b3a2")
-    // no bar at the beginning thus:
-    .attr("height", function(d) { return height - y(0); }) // always equal to 0
-    .attr("y", function(d) { return y(0); })
-
-// Animation
-svg.selectAll("rect")
-  .transition()
-  .duration(800)
-  .attr("y", function(d) { return y(d.Value); })
-  .attr("height", function(d) { return height - y(d.Value); })
-  .delay(function(d,i){console.log(i) ; return(i*100)})
-
-})
-</script>
-</div>
+            // Animation
+            svg.selectAll("rect")
+            .transition()
+            .duration(800)
+            .attr("y", function(d) { return y(d.YearAvgDayWater); })
+            .attr("height", function(d) { return height - y(d.YearAvgDayWater); })
+            .delay(function(d,i){console.log(i) ; return(i*100)})
+        </script>
+    </div>
 </body>
 </html>
--- mongodb/getdata.php
+++ mongodb/getdata.php
@@ -1,6 +1,7 @@
 <?php
     //지역의 개수와 num의 개수를 맞춰준다.
     require_once "vendor/autoload.php";
+    header("Content-Type:text/html;charset=utf-8");
     $num = $_GET['num'];
     $city = $_GET['city'];
     $city2 = $_GET['city2'];
@@ -8,6 +9,7 @@
     $city4 = $_GET['city4'];
     $collection = (new MongoDB\Client)->WeatherProjects->Weather;
     
+    $sendCity = $city;
     
     include "CityCode.php"; //지역 코드 가져오기
     
@@ -16,8 +18,6 @@
     $code3 = constant($city3);
     $code4 = constant($city4);
 
-    //지역별 데이터 개수
-    
     //지역별 데이터 평균
     function CityAvg($collection){
         $rst = array();
@@ -189,9 +189,105 @@
         //echo $cursor;
         return $cursor;
     }
+    //년도별 평균 일일 강수량
+    function YearDayWaterData($collection){
+        $rst = array();
+        $temp = array();
+        $cursor = $collection->aggregate([
+            ['$group' => [
+                '_id' => [
+                    'year'=> '$Year'],
+                'avg' => [
+                    '$avg'=> '$DayWater']
+                ]
+            ],
+            ['$sort'=> ['_id'=> 1]]
+        ]);
+        foreach ($cursor as $temp){
+            array_push($rst,
+            array(
+                'Year' => $temp[_id][year],
+                'YearAvgDayWater' => round($temp[avg],1)
+            ));
+        };
+        return $rst;
+
+        //출력 형식
+        //for ($row =0; $row < sizeof($rst); $row++){
+        //    echo "년 평균 강수량 : ".$rst[$row]['YearAvgDayWater']."&nbsp <br>";
+        //}
+    }
+    //지역별 일일 강수량 평균
+    function CityDayWaterData($collection, $code){
+        $rst = array();
+        $temp = array();
+        $cursor = $collection->aggregate([
+            [
+                '$match'=> [
+                    'City'=> $code
+                ]
+            ],
+            [
+                '$group' => [
+                    '_id' => [
+                        'city'=> '$City',
+                        'year'=> '$Year'
+                    ],
+                    'avg' => [
+                        '$avg'=> '$DayWater'
+                    ]
+                ]
+            ],
+            [
+                '$sort'=> ['_id'=> 1]
+            ]
+        ]);
+        
+        foreach ($cursor as $temp){
+            array_push($rst,
+            array(
+                'Year' => $temp[_id][year],
+                'CityAvgDayWater' => round($temp[avg],1)
+            ));
+        };
+        //출력 형식
+        //for ($row =0; $row < sizeof($rst); $row++){
+        //    echo "년 평균 강수량 : ".$rst[$row]['CityAvgDayWater']."&nbsp <br>";
+       // }
+        return $rst;
+    }
+    //지역별 강수량과 전체 지역 강수량 데이터 넣기
+    function CityYearAvgWaterData($collection, $city, $code){
+        $totalWaterData = YearDayWaterData($collection);
+        $CityWaterData = CityDayWaterData($collection, $code);
+        $data = array();
+        $temp = array();
+        $temp2 = array();
+        
+        for($a=0; $a<18; $a++){
+            array_push($data,
+            array(
+                'Year' => $totalWaterData[$a][Year],
+                'Values' => array(
+                    array(
+                        'value' => $totalWaterData[$a][YearAvgDayWater],
+                        'City' => 'Total'
+                    ),
+                    array(
+                        'value' => $CityWaterData[$a][CityAvgDayWater],
+                        'City' => "$city"
+                    )
+                )
+            )
+            );
+        }
+        return $data;
+    }
+    
     //CityYearAvg($collection, $code);
     //MonthAvgTemp($collection);
     //YearAvg($collection);
     //CityAvg($collection);
-    TotalData($collection);
+    //TotalData($collection);
+    //YearDayWaterData($collection);
 ?>
